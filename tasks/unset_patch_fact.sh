8fa562238ffddd6753ce8eb10d550d48c7e7ee85
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-07 09:57:27 +1000

    update

--- tasks/unset_patch_fact.sh
+++ tasks/unset_patch_fact.sh
@@ -4,7 +4,7 @@ export
 
 factfile=$PT_factfile
 factname=$PT_factname
-echo poodle $PT_factfile
+echo Testing123 $PT_factfile
 echo sed -i "s/$factname=true/$factname=false/g" $factfile
 
 sed -i "s/$factname=true/$factname=false/g" $factfile
