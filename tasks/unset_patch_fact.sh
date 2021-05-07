46f9393bfd32cad0f7840a62702b5c15944e37a1
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-07 10:43:17 +1000

    Add check if unset worked

--- tasks/unset_patch_fact.sh
+++ tasks/unset_patch_fact.sh
@@ -6,3 +6,6 @@ factname=$PT_factname
 echo sed -i "s/$factname=true/$factname=false/g" $factfile
 
 sed -i "s/$factname=true/$factname=false/g" $factfile
+
+#check the fact has been unset
+facter | grep 'patchme => false'
