0f2fd2f58a1262c725eecd8559cc83f5a116c24b
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-07 10:09:03 +1000

    added plan

---
+++ plans/first_patch.pp
@@ -0,0 +1,24 @@
+plan bolt::first_patch (
+  String $patch_group,
+) {
+
+  $nodes_to_patch = puppetdb_query("inventory[certname] { facts.patchme = \"true\" }")
+
+  #turn into targetspec
+  $filtered_nodes = $nodes_to_patch.map | $i | { $i['certname']}
+  $targets = get_targets($filtered_nodes)
+
+  $patch_results = run_task('bolt::unset_patch_fact',
+                            $targets,
+                            factfile => '/opt/puppetlabs/facter/facts.d/patchme.txt',
+                            factname => 'patchme',
+                  )
+
+
+  return({
+    'patch_results'  => $patch_results,
+    'nodes_to_patch' => $nodes_to_patch,
+    'filtered_nodes' => $filtered_nodes,
+    'targets'        => $targets,
+  })
+}
--- tasks/unset_patch_fact.sh
+++ tasks/unset_patch_fact.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-export
-
 factfile=$PT_factfile
 factname=$PT_factname
-echo Testing123 $PT_factfile
+
 echo sed -i "s/$factname=true/$factname=false/g" $factfile
 
 sed -i "s/$factname=true/$factname=false/g" $factfile
