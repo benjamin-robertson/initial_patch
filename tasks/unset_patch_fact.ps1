8b5eea18924ed385c9232c2aad4484bcf30454b7
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-10 10:25:24 +1000

    Update to use varibels

--- tasks/unset_patch_fact.ps1
+++ tasks/unset_patch_fact.ps1
@@ -3,5 +3,5 @@ echo $env:PT_factname
 #gci env:*
 
 # unset patchme
-(get-content C:\ProgramData\PuppetLabs\facter\facts.d\patchme.yaml).replace('patchme: true', 'patchme: false') | Set-Content C:\ProgramData\PuppetLabs\facter\facts.d\patchme.yaml
-#(get-content $env:PT_factfile).replace('$env:PT_factname: true', '$env:PT_factname: false') | Set-Content $env:PT_factfile
+#(get-content C:\ProgramData\PuppetLabs\facter\facts.d\patchme.yaml).replace('patchme: true', 'patchme: false') | Set-Content C:\ProgramData\PuppetLabs\facter\facts.d\patchme.yaml
+(get-content $env:PT_factfile).replace("$env:PT_factname: true", "$env:PT_factname: false") | Set-Content $env:PT_factfile
